30be360434566afe7e7dcafff671a9e4a80ae649
Katie <katie.bielawiec@gmail.com>
2016-02-18 01:54:55 +0000

    Added popups and links

--- public/js/googlemap.js
+++ public/js/googlemap.js
@@ -6,6 +6,7 @@ var map;
 var infoWindow;
 var placesArray = [];
 var currentPlace = 0;
+var infowin;
 
 function initMap() {
     'use strict';
@@ -69,6 +70,25 @@ function createEventMarker(map, infoWindow, name, type, lat, lng) {
             lng: longitude
         },
         name: name
+    });
+
+    var contentString = '<div id="content">'+
+      '<h3 id="firstHeading" class="firstHeading">' +
+      '<a href = "project.handlebars">' + name + '</a>' + '</h3>'+
+      '<div id="bodyContent">'+
+      '<p>'+ type+
+      '</p>' +
+      '</div>';
+    
+    infowin = new google.maps.InfoWindow({
+        map: map,
+        content: "Starting Content"
+    });
+
+    markerTemp.addListener('click', function() {
+        infowin.close();
+        infowin.setContent(contentString);
+        infowin.open(map, markerTemp); 
     });
 }
 
